d82c18e67ce4a450a1a71104ce97855b55ea35b3
Kanthamas <114124443+Kanthamas@users.noreply.github.com>
2025-04-28 22:12:39 +0700

    Add userRoutesMongo.test.js POST /auth/register (edge case and negative path)

--- tests/routes/userRoutesMongo.test.js
+++ tests/routes/userRoutesMongo.test.js
@@ -5,7 +5,6 @@ import {
 import request from "supertest";
 import app from "../../app.js";
 import User from "../../models/user.model.js";
-import { describe } from "vitest";
 
 beforeAll(async () => {
 	await startMongoServer();
@@ -26,6 +25,7 @@ afterEach(async () => {
 // });
 
 describe("POST /api/v1/users/auth/register", () => {
+	const endpoint = "/api/v1/users/auth/register";
 	describe("Happy path: Given valid data", () => {
 		it("should create a user successfully", async () => {
 			const newUser = {
@@ -34,21 +34,51 @@ describe("POST /api/v1/users/auth/register", () => {
 				password: "iamjohnwick",
 			};
 
-			const response = await request(app)
-				.post("/api/v1/users/auth/register")
-				.send(newUser);
+			const response = await request(app).post(endpoint).send(newUser);
 
 			expect(response.status).toBe(201);
 			expect(response.body.message).toBe("Register a new user successfully");
 			expect(response.body.data).toBe(newUser.email);
 
-			// Confirm the user is saved in DB
+			// Confirm the user is saved in DB //
 			const userInDb = await User.findOne({ email: newUser.email });
-      // console.log(userInDb)
+			// console.log(userInDb)
 			expect(userInDb).not.toBeNull();
 			expect(userInDb.name).toBe(newUser.name);
 		});
 	});
-	// describe("Edge cases: Given duplicate email", () => {});
-	// describe("Negative path: Missing input", () => {});
+
+	describe("Edge cases: Given duplicate email", () => {
+		it("should return an error if email is already used", async () => {
+			const existingUser = new User({
+				name: "Bob Smith",
+				email: "bob@example.com",
+				password: "anotherPassword",
+			});
+			await existingUser.save();
+
+			const response = await request(app).post(endpoint).send({
+				name: "Bob Smith Duplicate",
+				email: "bob@example.com",
+				password: "newPassword",
+			});
+
+			expect(response.status).toBe(409);
+			expect(response.body.error).toBe(true);
+			expect(response.body.message).toBe("Email already used");
+		});
+	});
+
+	describe("Negative path: Missing input", () => {
+		it("should return an error if required fields are missing (Negative Path)", async () => {
+			const response = await request(app).post(endpoint).send({
+				email: "missing@name.com",
+				// Missing name and password
+			});
+
+			expect(response.status).toBe(400);
+			expect(response.body.error).toBe(true);
+			expect(response.body.message).toBe("All fields are required");
+		});
+	});
 });
